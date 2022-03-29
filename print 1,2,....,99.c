103dd3807e99e277550e1f4ca0d2f3bae3f9eeb2
Md Alhaz <101819303+alhaz1077@users.noreply.github.com>
2022-03-29 21:22:56 +0600

    print 1,2,....,99 done

---
+++ print 1,2,....,99.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+
+    int i,n;
+
+    printf("Enter a number :");
+    scanf("%d",&n);
+
+    for(i=1;i<=n;i++)
+    {
+        printf("%d",i);
+        if(i!=n)
+        {
+            printf(",");
+        }
+    }
+
+    return 0;
+}
+
