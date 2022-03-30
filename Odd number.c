4f4380816b303848f871910364e2d8b550c93f88
Md Alhaz <101819303+alhaz1077@users.noreply.github.com>
2022-03-30 11:00:11 +0600

    Odd number print done

---
+++ Odd number.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    int n,i;
+
+    printf("enter a number :");
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {
+        printf("%d",i);
+        i++;
+        if(i!=n)
+        {
+            printf(",");
+        }
+    }
+    return 0;
+}
