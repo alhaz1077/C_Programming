f541ba5ac4829d42e1bbb89629a44f3fe272d6c7
Md Alhaz <101819303+alhaz1077@users.noreply.github.com>
2022-03-30 09:57:52 +0600

    Even number print

---
+++ print 2,4,6,...N.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+    int n,i;
+    printf("Enter a number :");
+    scanf("%d",&n);
+
+    for(i=2;i<=n;i++)
+    {
+        printf("%d",i);
+        i++;
+
+        if(i!=n)
+        {
+            printf(",");
+        }
+    }
+
+    return 0;
+}
