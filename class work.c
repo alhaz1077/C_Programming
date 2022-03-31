5bc64894f80440edb04d94c0eb4dd628da057350
Md Alhaz <101819303+alhaz1077@users.noreply.github.com>
2022-03-31 11:22:17 +0600

    sum of two number

---
+++ class work.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int A=5,B=5,SUM;
+
+    printf("Enter two intiger number :");
+    scanf("%d %d",&A,&B);
+
+    SUM=A+B;
+    printf("The sum is =%d\n",SUM);
+
+
+
+    return 0;
+}
