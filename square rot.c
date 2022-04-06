2043987465135114a6dd1cdff96499e314ec944c
Md Alhaz <101819303+alhaz1077@users.noreply.github.com>
2022-04-06 14:11:45 +0600

    square rot

---
+++ square rot.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    float number,ans;
+    printf("Enter a number :");
+    scanf("%f",&number);
+
+    ans = sqrt(number);
+    printf("Ans is = %.2f",ans);
+
+
+    return 0;
+}
