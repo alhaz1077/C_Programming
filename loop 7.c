ac7ad21016a581e4bf2fcf7e77da55b16fd11b37
Md Alhaz <101819303+alhaz1077@users.noreply.github.com>
2022-04-20 13:41:43 +0600

    sum of all natural number

---
+++ loop 7.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+    int i, n, sum=0;
+
+    printf("Enter upper limit: ");
+    scanf("%d", &n);
+
+    for(i=1; i<=n; i++)
+    {
+        sum =sum+i;
+        printf("%d + %d\n",i,sum);
+    }
+
+    printf("Sum is = %d", sum);
+
+    return 0;
+}
